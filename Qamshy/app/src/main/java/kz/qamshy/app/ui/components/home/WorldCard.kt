bb439244271be4139c96df64ccb0f251fa041b74
alem2coder1 <148032941+alem2coder1@users.noreply.github.com>
2025-04-26 20:34:12 +0500

    djh

--- Qamshy/app/src/main/java/kz/qamshy/app/ui/components/home/WorldCard.kt
+++ Qamshy/app/src/main/java/kz/qamshy/app/ui/components/home/WorldCard.kt
@@ -3,24 +3,32 @@ package kz.qamshy.app.ui.components.home
 import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
@@ -32,17 +40,37 @@ import kz.qamshy.app.models.site.ArticleModel
 import kz.qamshy.app.ui.theme.PrimaryFontFamily
 
 @Composable
-fun WorldCard(
+fun WorldCardRow(
     currentLanguage: LanguageModel,
     context: Context,
-    article: ArticleModel
-){
+    articleBlock: ArticleBlockModel
+) {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(12.dp) // 卡片之间的间距
+    ) {
+        item{
+            Spacer(modifier = Modifier.width(12.dp))
+        }
+        items(articleBlock.articleList) { article ->
+            WorldCard(article)
+        }
+        item{
+            Spacer(modifier = Modifier.width(12.dp))
+        }
+    }
+}
+
+@Composable
+fun WorldCard(article: ArticleModel) {
     Box(
         modifier = Modifier
-            .fillMaxWidth()
+            .width(280.dp)
             .height(173.dp)
             .clip(RoundedCornerShape(10.dp))
+            .clickable { /* 处理点击事件 */ }
     ) {
+        // 图片背景
         val painter = rememberAsyncImagePainter(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(article.thumbnailUrl)
@@ -52,14 +80,29 @@ fun WorldCard(
 
         Image(
             painter = painter,
-            contentDescription = "pinned image",
+            contentDescription = "article image",
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black.copy(alpha = 0.7f)
+                        ),
+                        startY = 100f
+                    )
+                )
+        )
+
         Column(
             modifier = Modifier
                 .align(Alignment.BottomStart)
-                .padding(horizontal = 16.dp, vertical = 10.dp),
+                .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Text(
@@ -68,8 +111,9 @@ fun WorldCard(
                 fontFamily = PrimaryFontFamily,
                 fontWeight = FontWeight(600),
                 color = Color.White,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
             )
         }
-
     }
 }
--- Qamshy/app/src/main/java/kz/qamshy/app/ui/screens/HomeScreen.kt
+++ Qamshy/app/src/main/java/kz/qamshy/app/ui/screens/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -17,11 +18,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -35,8 +38,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -55,6 +61,8 @@ import kz.qamshy.app.ui.components.global.EmptyCard
 import kz.qamshy.app.ui.components.home.FocusCard
 import kz.qamshy.app.ui.components.home.PinnedCard
 import kz.qamshy.app.ui.components.home.WorldCard
+import kz.qamshy.app.ui.components.home.WorldCardRow
+import kz.qamshy.app.ui.theme.PrimaryFontFamily
 import kz.qamshy.app.ui.theme.darkColor
 
 
@@ -131,27 +139,52 @@ fun HomeScreen(context: Context, isDarkMode:Boolean, viewModel: HomeViewModel) {
                                                     state = listState,
                                                     modifier = Modifier
                                                         .fillMaxSize()
-                                                        .padding(horizontal = 20.dp),
+                                                       ,
                                                     verticalArrangement = Arrangement.spacedBy(6.dp)
                                                 ) {
+
                                                     items(articleList.pinnedArticleList){ article ->
-                                                        PinnedCard(currentLanguage,context,article)
+                                                        Box(modifier = Modifier.padding(horizontal = 20.dp)){
+                                                            PinnedCard(currentLanguage,context,article)
+                                                        }
+
                                                     }
                                                     item{
-                                                        FocusCard(currentLanguage,context,articleList.focusArticleList)
+                                                        Box(modifier = Modifier.padding(horizontal = 20.dp)){
+                                                            FocusCard(currentLanguage,context,articleList.focusArticleList)
+                                                        }
+
                                                     }
-                                                    item {
-                                                        articleList.blockList.firstOrNull()?.let { firstBlock ->
-                                                            LazyRow() {
-                                                                items(firstBlock.articleList) { article ->
-                                                                    WorldCard(
-                                                                        currentLanguage,
-                                                                        context,
-                                                                        article
+                                                    itemsIndexed(articleList.blockList) { index, articleBlock ->
+                                                        Spacer(modifier = Modifier.height(25.dp))
+                                                        when(index){
+                                                            0 ->
+                                                                Column(
+                                                                    modifier = Modifier
+                                                                        .fillMaxWidth()
+                                                                ) {
+                                                                    Text(
+                                                                        text = articleBlock.categoryTitle,
+                                                                        style = TextStyle(
+                                                                            fontSize = 16.sp,
+                                                                            lineHeight = 20.sp,
+                                                                            fontFamily = PrimaryFontFamily,
+                                                                            fontWeight = FontWeight(600),
+                                                                            color = Color(0xFF000000),
+                                                                        ),
+                                                                        modifier = Modifier.padding(bottom = 13.dp).padding(horizontal = 20.dp)
+                                                                    )
+
+                                                                    WorldCardRow(
+                                                                        currentLanguage = currentLanguage,
+                                                                        context = context,
+                                                                        articleBlock = articleBlock
                                                                     )
                                                                 }
-                                                            }
+
                                                         }
+
+
                                                     }
 
 
